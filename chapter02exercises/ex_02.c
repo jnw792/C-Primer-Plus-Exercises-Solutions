a845258c62ec799eb4571a3d7a17f758715fa9b3
jnw792 <jnw792@outlook.com>
2019-09-03 21:00:13 -0500

    Did chapter 2 exercise 2

---
+++ chapter02exercises/ex_02.c
@@ -0,0 +1,14 @@
+/*
+  Programming exercise 2-2
+  Write a program to print your name and address
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("Love Snowball \n");
+  printf("1 E Jackson Blvd, Chicago, IL 60604 \n");
+
+  return 0;
+}
